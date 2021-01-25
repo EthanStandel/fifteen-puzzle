0682d00c7922f345c9c7212b4a3ff982a0be8ed9
Ethan Standel <ethsta2013@gmail.com>
2021-01-25 15:15:08 -0500

    Add translations to spread pieces out into their initial state.

--- src/components/PlayPiece.tsx
+++ src/components/PlayPiece.tsx
@@ -4,22 +4,32 @@ import { BufferSpace } from '../shared/BufferSpace';
 export interface Props {
   id: number;
   edgeLength: number;
+  translateX: number;
+  translateY: number;
 }
 
 /**
  * Really just the simple display logic for a play piece.
  * All the movement and game state is handled by the PlaceSpace component.
  */
-export const PlayPiece: React.FC<Props> = ({ id, edgeLength }) => {
+export const PlayPiece: React.FC<Props> = ({
+  id,
+  edgeLength,
+  translateX,
+  translateY
+}) => {
   return <div style={{
     position: "absolute",
-    height: edgeLength,
-    width: edgeLength,
+    border: "solid 5px silver",
+    transform: `translate(${translateX}px, ${translateY}px)`,
+    height: edgeLength - 10,
+    width: edgeLength - 10,
     backgroundColor: id % 2 === 0 ? "beige" : "firebrick",
     borderRadius: BufferSpace
   }}>
     <strong style={{
       display: "block",
+      userSelect: "none",
       textAlign: "center",
       lineHeight: `${edgeLength}px`, // default is not px here
       color: "silver",
--- src/components/PlaySpace.tsx
+++ src/components/PlaySpace.tsx
@@ -9,7 +9,8 @@ export interface Props {
 export const PlaySpace: React.FC<Props> = ({ edgeLength }) => {
   const playPieceEdgeLength = edgeLength / 4;
 
-  const [ playPieceIds ] = useState(
+
+  const [ gameState ] = useState(
     Array.from({ length: 15 })
       .map((_val, index) => index + 1)
       .sort(() => Math.random() > .5 ? -1 : 1) // randomize pieces
@@ -22,9 +23,13 @@ export const PlaySpace: React.FC<Props> = ({ edgeLength }) => {
         backgroundColor: "aliceblue",
         borderRadius: BufferSpace
     }}>
-      {playPieceIds.map(id =>
-        <PlayPiece edgeLength={playPieceEdgeLength} id={id} key={id} />
-      )}
+      {gameState.map((id, index) => {
+        return <PlayPiece edgeLength={playPieceEdgeLength} 
+                   translateX={(index % 4) * playPieceEdgeLength}
+                   translateY={Math.floor(index / 4) * playPieceEdgeLength}
+                   id={id} 
+                   key={id} />
+      })}
     </div>
   );
 }
